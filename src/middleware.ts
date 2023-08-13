a38bd8436dbce008a86d62bec912ba6c70d9a584
hayato94087 <hayato94087@gmail.com>
2023-08-14 07:27:21 +0900

    fix log

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,7 +42,7 @@ export function middleware(request: NextRequest) {
       ip: request.ip,
       message: `IPアドレス ${request.ip} からのアクセスは拒否されました。`,
     };
-    console.warn(log);
+    console.log(log);
     return new NextResponse(null, { status: 401 });
   }
 
