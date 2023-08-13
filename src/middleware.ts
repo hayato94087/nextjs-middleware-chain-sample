c152453bf0ea3718175ce00e803fb8c21c26f0e9
hayato94087 <hayato94087@gmail.com>
2023-08-14 08:03:25 +0900

    Higher-Order Function（高階関数）を定義し、ファイルを分割

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,58 +1,65 @@
-import { NextRequest, NextResponse } from "next/server";
+import {
+  NextFetchEvent,
+  NextMiddleware,
+  NextRequest,
+  NextResponse,
+} from "next/server";
 
-// This function can be marked `async` if using `await` inside
-async function loggingMiddleware(request: NextRequest) {
-  // ##################################################
-  // ログ出力
-  //
-  // パスが以下の場合にログを出力します。
-  // ・"/"から始まり、"."を含まない任意のパス
-  // ・"/_nextから始まらない任意のパス
-  // ・"/"のルートパス。
-  // ・"/api"から始まる任意のパス
-  // ・"/trpc"から始まる任意のパス
-  // ##################################################
-  if (
-    request.nextUrl.pathname.match(/\/(?!.*\..*|_next).*/) ||
-    request.nextUrl.pathname.match(/\/(api|trpc)(.*)/) ||
-    request.nextUrl.pathname === "/"
-  ) {
-    // リクエストの情報をJSON形式で出力します。
-    const log = {
-      ip: request.ip,
-      geo: request.geo,
-      url: request.nextUrl.pathname,
-    };
-    console.log(JSON.stringify(log, (k, v) => (v === undefined ? null : v)));
-  }
+function widthLogging(middleware: NextMiddleware) {
+  return async (request: NextRequest, event: NextFetchEvent) => {
+    // ##################################################
+    // ログ出力
+    //
+    // パスが以下の場合にログを出力します。
+    // ・"/"から始まり、"."を含まない任意のパス
+    // ・"/_nextから始まらない任意のパス
+    // ・"/"のルートパス。
+    // ・"/api"から始まる任意のパス
+    // ・"/trpc"から始まる任意のパス
+    // ##################################################
+    if (
+      request.nextUrl.pathname.match(/\/(?!.*\..*|_next).*/) ||
+      request.nextUrl.pathname.match(/\/(api|trpc)(.*)/) ||
+      request.nextUrl.pathname === "/"
+    ) {
+      // リクエストの情報をJSON形式で出力します。
+      const log = {
+        ip: request.ip,
+        geo: request.geo,
+        url: request.nextUrl.pathname,
+      };
+      console.log(JSON.stringify(log, (k, v) => (v === undefined ? null : v)));
+    }
+
+    return middleware(request, event);
+  };
 }
 
-// This function can be marked `async` if using `await` inside
-async function ipRestrictionMiddleware(request: NextRequest) {
-  // ##################################################
-  // IP制限
-  //
-  // 全てのパスに対してIP制限を実行します。
-  // ##################################################
-  // ホワイトリストに登録されたIPリストを取得します。
-  const ipWhiteList = new Set(
-    process.env.IP_WHITE_LIST?.split(",").map((item: string) => {
-      return item.trim();
-    })
-  );
-  // ホワイトリストに登録されていないIPアドレスからのアクセスは拒否します。
-  if (request.ip && !ipWhiteList.has(request.ip as string)) {
-    const log = {
-      message: `許可されていないIPアドレスからのアクセスのためアクセスを拒否しました`,
-      ip: request.ip,
-    };
-    console.log(log);
-    return new NextResponse(null, { status: 401 });
-  }
+function widthIpRestriction(middleware: NextMiddleware) {
+  return async (request: NextRequest, event: NextFetchEvent) => {
+    // ##################################################
+    // IP制限
+    //
+    // 全てのパスに対してIP制限を実行します。
+    // ##################################################
+    // ホワイトリストに登録されたIPリストを取得します。
+    const ipWhiteList = new Set(
+      process.env.IP_WHITE_LIST?.split(",").map((item: string) => {
+        return item.trim();
+      })
+    );
+    // ホワイトリストに登録されていないIPアドレスからのアクセスは拒否します。
+    if (request.ip && !ipWhiteList.has(request.ip as string)) {
+      const log = {
+        message: `許可されていないIPアドレスからのアクセスのためアクセスを拒否しました`,
+        ip: request.ip,
+      };
+      console.log(log);
+      return new NextResponse(null, { status: 401 });
+    }
+
+    return middleware(request, event);
+  };
 }
 
-export async function middleware(request: NextRequest) {
-  await loggingMiddleware(request);
-  await ipRestrictionMiddleware(request);
-  return NextResponse.next();
-}
+export default widthLogging(widthIpRestriction(() => NextResponse.next()));
