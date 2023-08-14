74ada73b8defe7f60a364724502109f102f1ade9
hayato94087 <hayato94087@gmail.com>
2023-08-14 10:58:51 +0900

    より簡潔に記載するために関数をチェインする方法を実装

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,8 @@
-import { NextResponse } from "next/server";
 import { widthLogging } from "@/middlewares/withLogging";
 import { widthIpRestriction } from "@//middlewares/widthIpRestriction";
+import { chainMiddlewares } from "@/middlewares/chain";
 
-export default widthLogging(widthIpRestriction(() => NextResponse.next()));
+// 関数は配列の順番に実行されます。
+// ・全てのアクセスに対してログを出力します。
+// ・IP制限を実行します。
+export default chainMiddlewares([widthLogging, widthIpRestriction]);
